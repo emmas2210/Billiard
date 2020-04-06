f495202b4d5022d366c863adcd54a9f2e2de123c
selenaiskounen <60702138+selenaiskounen@users.noreply.github.com>
2020-04-06 14:39:05 +0200

    ajout de fonctions tests

--- test.py
+++ test.py
@@ -1,2 +1,16 @@
 
-#Function test for square billiard:
+#Functions tests for square billiard:
+from billiardpack import *
+
+def test_deplacement ():
+    dx=random.randint(3,8)
+    dy=random.randint(5,10)
+    deplacement()
+
+
+
+def test_collision():
+    X2==X3== random.randint(2,3)
+    Z0==Z1== random.randint(2,3)
+
+print(collision)
