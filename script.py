7c3cf2114739cd3da163d01d9264583055e61d67
selenaiskounen <60702138+selenaiskounen@users.noreply.github.com>
2020-04-26 00:09:41 +0200

    change the script

--- script.py
+++ script.py
@@ -1,62 +1,74 @@
-import tkinter
-import random
-
-window = tkinter.Tk()
-tk = Tk()
-tk.title("Square Billiard Game")
-label_title=Label(tk,text="Welcome to billiard game")
-label_title.pack()
-canvas = tkinter.Canvas(tk, width="400", height="400",background='#096a09') #Dimension square are 400x400
-canvas.pack(padx=10,pady=10)
-
-balle1 = canvas.create_oval(10,10,30,30,fill='white')
-
-dx=3
-dy=5
-
+from tkinter import *
+import math,random
+LARGEUR = 480
+HAUTEUR = 320
+RAYON = 15 # rayon de la balle
+collision =str()
+# position initiale au milieu
+X = LARGEUR/2
+Y = HAUTEUR/2
+# direction initiale aléatoire
+vitesse = random.uniform(1.8,2)*5
+angle = random.uniform(0,2*math.pi)
+DX = vitesse*math.cos(angle)
+DY = vitesse*math.sin(angle)
 def deplacement():
-     global dx, dy
-if (canvas.coords(balle1)[3]>400):
-    dy=-1*dy
-if (canvas.coords(balle1)[2]>400):
-    dx=-1*dx
-if (canvas.coords(balle1)[1]<0):
-    dy=-1*dy
-if (canvas.coords(balle1)[0]<0):
-    dx=-1*dx
-            
-    #Ball's movement :
-canvas.move(balle1,dx,dy)
-    #We reiterate this function 
-tk.after(20,deplacement)
- 
-Bouton_Quitter=Button(tk, text ='Quit', command = tk.destroy)
+    """ Déplacement de la balle """
+    global X,Y,DX,DY,RAYON,LARGEUR,HAUTEUR,collision
+    # rebond à droite
+    if X+RAYON+DX > LARGEUR:
+        X = 2*(LARGEUR-RAYON)-X
+        DX = -DX
+        collision+='V'
+    # rebond à gauche
+    if X-RAYON+DX < 0:
+        X = 2*RAYON-X
+        DX = -DX
+        collision+='V'
+    # rebond en bas
+    if Y+RAYON+DY > HAUTEUR:
+        Y = 2*(HAUTEUR-RAYON)-Y
+        DY = -DY
+        collision+='H'
+    # rebond en haut
+    if Y-RAYON+DY < 0:
+        Y = 2*RAYON-Y
+        DY = -DY
+        collision+='H'
+    X = X+DX
+    Y = Y+DY
+    print(collision)
+    # affichage
+    Canevas.coords(Balle,X-RAYON,Y-RAYON,X+RAYON,Y+RAYON)
+    # mise à jour toutes les 50 ms
+    Mafenetre.after(20,deplacement)
+# Création de la fenêtre principale
+Mafenetre = Tk()
+Mafenetre.title("Animation Balle")
+label_title=Label(Mafenetre,text="Welcome to billiard game")
+label_title.pack()
+# Création d'un widget Canvas
+Canevas = Canvas(Mafenetre,height=HAUTEUR,width=LARGEUR,bg='green')
+Canevas.pack(padx=5,pady=5)
+# Création d'un objet graphique
+Balle = Canevas.create_oval(X-RAYON,Y-RAYON,X+RAYON,Y+RAYON,width=1,fill='white')
+#Bouton quitter
+Bouton_Quitter=Button(Mafenetre, text ='Quit', command = Mafenetre.destroy)
 Bouton_Quitter.pack()
-  
+
 deplacement()
+Mafenetre.mainloop()
 
 
+from mayavi import mlab
+mlab.init_notebook(backend = 'x3d' , local=False)
 
-collision = str()
+theta=np.linspace(0, 2*np.pi,100)
+phi= np.linspace(0, 2*np.pi,100)
 
-X2= (canvas.coords(balle1)[2]== 400)
-X3= (canvas.coords(balle1)[3]== 400)
-Z0= (canvas.coords(balle1)[0] == 0)
-Z1= (canvas.coords(balle1)[1] == 0)
-        
+X = np.outer((1+0.2*np.cos(phi)),np.cos(theta))
+Y = np.outer((1+0.2*np.cos(phi)),np.sin(theta))
+Z = np.outer(np.sin(phi), 0.09*np.ones(np.size(theta)))
 
-while len(collision)<200:
-        X2=X3=random.randint(0,1)
-        Z0=Z1=random.randint(0,1)
-        if X2==X3==0:
-            collision += 'h'
-        if X2==X3==1:
-            collision
-        if Z0==Z1==0:
-            collision += 'v'
-        if Z0==Z1==1:
-            collision
-
-
-print(collision)
-tk.mainloop()
+mlab.clf()
+mlab.mesh(X,Y,Z, representation='wireframe')
