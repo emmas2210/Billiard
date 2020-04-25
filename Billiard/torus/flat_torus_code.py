b6fc6f2f090f001a32f1134b6cd9aef6a32b7206
selenaiskounen <60702138+selenaiskounen@users.noreply.github.com>
2020-04-26 00:09:04 +0200

    some changes

--- Billiard/torus/flat_torus_code.py
+++ Billiard/torus/flat_torus_code.py
@@ -1,4 +1,17 @@
 #code du torrus billiard
+from mayavi import mlab
+mlab.init_notebook(backend = 'x3d' , local=False)
+
+theta=np.linspace(0, 2*np.pi,100)
+phi= np.linspace(0, 2*np.pi,100)
+
+X = np.outer((1+0.2*np.cos(phi)),np.cos(theta))
+Y = np.outer((1+0.2*np.cos(phi)),np.sin(theta))
+Z = np.outer(np.sin(phi), 0.09*np.ones(np.size(theta)))
+
+mlab.clf()
+mlab.mesh(X,Y,Z, representation='wireframe')
+
 import pygame 
 from pygame.locals import*
 from OpenGL.GL import*
