c676c1da0db0e243b4656eb6be9f02c55767fb73
selenaiskounen <60702138+selenaiskounen@users.noreply.github.com>
2020-05-15 16:32:41 +0200

    change

--- Billiard/__init__.py
+++ Billiard/__init__.py
@@ -1,4 +1,9 @@
 __version__='0.0.1'
 
-from .square_billiard_code import*
-from .flat_torus_code import*
+
+from .square.class_square_billiard import Billiard
+from .torus.class_flat_torus import flat_torus
+from .torus.function_flat_case import *
+from .torus.animation_3d import *
+from .torus.figure_mayavi import * 
+from .torus.toru_matplot import *
